b46fdab32ed08a3bd2d87c8b35bc7134d1894685
kaindick <kristin.a.indick@gmail.com>
2023-02-18 15:36:24 -0800

    fix routes

--- give-4-good/src/App.js
+++ give-4-good/src/App.js
@@ -1,31 +1,66 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
+import Home from "./components/Home";
+
 
 function App() {
   return (
+
     <div>
       <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
+       <Route path="/home" element={<Home />} />
+    </Routes>
     </div>
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
+    // <div className="home-container">
+
+    //   {/* LOGO */}
+    //   <nav>
+    //     <a href="#" class="logo">
+    //       <h1> LOGO </h1>
     //     </a>
-    //   </header>
+
+    //     {/* Pages */}
+    //     <ul>
+    //       <li class="nav-item">
+    //         {/* <a href="#home" class="nav-link" id="nav-link" target="content">Home</a> */}
+    //         <a href="#home" class="nav-link" id="nav-link" target="content">Home</a>
+    //       </li>
+    //       <li class="nav-item">
+    //         <a href="#about" class="nav-link" id="nav-link" target="content">About</a>
+    //       </li>
+    //       <li class="nav-item">
+    //         <a href="#map" class="nav-link" id="nav-link" target="content">Map</a>
+    //       </li>
+    //       <li class="nav-item">
+    //         <a href="#ngo" class="nav-link" id="nav-link" target="content">NGO</a>
+    //       </li>
+    //     </ul>
+    //   </nav>
+
+    //   {/* Home Section */}
+    //   {/* <section class="home">
+    //     <div class="home-section">
+    //       <h1>Home</h1>
+    //     </div>
+    //   </section> */}
+
+    //   {/* About Section */}
+    //   {/* <section class="about">
+    //     <div class="about-section">
+    //       <h1>About</h1>
+    //     </div>
+    //   </section> */}
+
+    //   {/* Map Section */}
+    //   <section class="map">
+    //     <div class="map-section">
+    //     </div>
+    //   </section>
+
+
     // </div>
+
   );
 }
 
