04a334a296a24e9456489b95530bb3c1edfaef99
kaindick <kristin.a.indick@gmail.com>
2023-02-18 14:50:10 -0800

    add router

---
+++ give-4-good/src/components/Home.js
@@ -0,0 +1,12 @@
+export default function Home() {
+    return(
+        // <div className="home-container">
+        //     <div className="title">
+        //         <h1>Home</h1>
+        //     </div>
+        // </div>
+        <div>
+            <p>HELLOOOOO</p>
+        </div>
+    );
+}
