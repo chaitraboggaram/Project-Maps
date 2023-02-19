6b4a736cd144112cf85a2083e400ce3324f93499
chaitraboggaram <cboggaram@scu.edu>
2023-02-18 19:49:35 -0800

    Login with O Auth

--- give-4-good/src/components/Login.js
+++ give-4-good/src/components/Login.js
@@ -2,9 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import {Button} from 'react-bootstrap';
-
+import React from 'react';
+import GoogleLogin from 'react-google-login';
 
 function clickButton() {
+    function App() {
+        const responseGoogle = (response) => {
+            console.log(response);
+            // You can make an API call to your server with the response.tokenId to verify the user's identity.
+        };
+
+        return (
+        <div className="App">
+        <GoogleLogin
+            clientId="YOUR_CLIENT_ID.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />
+        </div>
+        );
+    }
+
+export default App;
+
     alert('You clicked me!');
   }
   
